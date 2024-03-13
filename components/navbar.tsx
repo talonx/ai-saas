7d132cc985b57815a1fe659e9859632bd5708bb7
Hrishikesh Barua <talonx@gmail.com>
2024-03-13 12:44:52 +0530

    Modal dialog for upgrading on exceeding API usage limits

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,15 @@
 import { UserButton } from "@clerk/nextjs";
 import MobileSidebar from "@/components/mobile-sidebar";
+import { getApiUsage } from "@/lib/api-limit";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const apiLimitCount = await getApiUsage();
+
     return (
         <div className="flex items-center p-4">
-            <MobileSidebar />
+            <MobileSidebar 
+                apiLimitCount = {apiLimitCount}
+            />
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/"/>
             </div>
